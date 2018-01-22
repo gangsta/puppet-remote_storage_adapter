260c4ab35b3d250d2d0de72b154911328d14b511
kalinux <kazein09@gmail.com>
2018-01-22 12:49:01 +0100

    fix config

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,17 +3,17 @@
 ###
 class remote_storage_adapter::config (
   $binary_path         = undef,
-  $graphite_address,   = undef,
-  $graphite_prefix,    = undef,
-  $graphite_transport, = undef,
-  $influxdb_url,       = undef,
-  $influxdb_database,  = undef,
-  $influxdb_retention, = undef,
-  $influxdb_username,  = undef,
-  $opentsdb_url,       = undef,
-  $send_timeout,       = undef,
-  $web_listen_address, = undef,
-  $web_telemetry_path, = undef,
+  $graphite_address    = undef,
+  $graphite_prefix     = undef,
+  $graphite_transport  = undef,
+  $influxdb_url        = undef,
+  $influxdb_database   = undef,
+  $influxdb_retention  = undef,
+  $influxdb_username   = undef,
+  $opentsdb_url        = undef,
+  $send_timeout        = undef,
+  $web_listen_address  = undef,
+  $web_telemetry_path  = undef,
 ) {
   file { "$remote_storage_adapter::config::binary_path/remote_storage_adapter":
     source => "puppet:///modules/remote_storage_adapter/remote_storage_adapter"
